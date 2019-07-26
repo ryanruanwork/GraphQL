d8795a6bfdd64b925be93cd4f5ad93fd621894d4
Ryan Ruan <ryanruanwork@gmail.com>
2019-07-26 13:56:53 -0700

    song detail and nav

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import graphql from 'react-apollo';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
     render() {
+        const { song } = this.props.data;
+
+        if (!song) { return <div></div>; }
+
         return (
             <div>
-                <h3>Song Detail</h3>
+                <Link to="/">Back</Link>
+                <h3>{song.title}</h3>
             </div>
         )
     }
--- Lyrical-GraphQL/client/queries/fetchSong.js
+++ Lyrical-GraphQL/client/queries/fetchSong.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-    query SongQuery($id: ID!){
-        song(id: $id){
+    query SongQuery($id: ID!) {
+        song(id: $id) {
             id
             title
         }
